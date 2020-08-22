7dbde500018fbf2898fd315eff43a867737190fb
annie_truex <artruex@gmail.com>
2020-08-22 12:14:09 -0700

    add validations & relationships to user model

--- rti-backend/app/models/user.rb
+++ rti-backend/app/models/user.rb
@@ -3,12 +3,20 @@ has_one_attached :avatar
 
 has_secure_password
 
+has_many :user_interests, dependent: :destroy
+has_many :interests, through: :user_interests
+
 has_many :sent_messages, class_name: "Message", dependent: :destroy
 has_many :received_messages, class_name: "Message", foreign_key: "match_id", dependent: :destroy
 
 has_many :messages
 
-validates_presence_of :username, :email
+validates_presence_of :username, :email, :gender
+validates_presence_of :interests, :length => {:minuim => 2}
+validates_uniqueness_of :email, :username 
+validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+before_create :set_default_avatar
 
 def set_default_avatar
     if self.gender == "Male"
@@ -16,8 +24,23 @@ def set_default_avatar
     elsif self.gender == "Female" 
       self.image = "https://www.pngkit.com/png/detail/55-554008_woman-head-people-avatar-men-icon-png.png"
     elsif self.gender == "Prefer not to answer"
-      self.image = "https://dlp2gfjvaz867.cloudfront.net/product_photos/11302471/locked-hearts-and-hand-grenades-cute-wallpapers-heart-images_original.jpg"
+      self.image = "https://www.pngitem.com/pimgs/m/272-2720607_this-icon-for-gender-neutral-user-circle-hd.png"
     end
 end
+
+def find_my_matches 
+    users = User.joins(:user_interests).where('user_interests.interest_id IN (?)', self.interest_ids).where.not(id: self.id)
+    if self.orientation == "Straight"
+      matches = users.select do |user|
+        user.gender != self.gender && self.orientation == user.orientation
+      end
+      matches.uniq
+    elsif self.orientation == "Lesbian" || self.orientation == "Gay"
+      matches = users.select do |user|
+        user.gender == self.gender && self.orientation == user.orientation
+      end
+      matches.uniq
+    end
+  end
 
 end
