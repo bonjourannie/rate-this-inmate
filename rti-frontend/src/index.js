99bb39134140f212c3b5ece01e8c3d932175ee9b
annie_truex <artruex@gmail.com>
2020-09-09 12:35:22 -0700

    set up index.js and create store

--- rti-frontend/src/index.js
+++ rti-frontend/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'redux-store' //any component wrapped in Provider gets access to store
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 
 import App from './App';
 
-// set up store here {}
+// store holds (stores) data globally
+// reducer tells you what to do with/update store based on actions
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
